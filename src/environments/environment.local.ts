89fa050164ca945dc527de330bdc6be33d7813fb
RaihanaMJ <raihana.moksin@gmail,com>
2018-04-30 12:01:03 +0800

    Adding my own firestore

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -2,11 +2,11 @@ export const CONF_LOCAL = {
   production: false,
   environment: 'LOCAL',
   firebase: {
-    apiKey: "AIzaSyC5IyyAgjY4Ruqn_RecBiWlA-QcDjWiEEc",
-    authDomain: "wadt-asgn-3.firebaseapp.com",
-    databaseURL: "https://wadt-asgn-3.firebaseio.com",
-    projectId: "wadt-asgn-3",
-    storageBucket: "wadt-asgn-3.appspot.com",
-    messagingSenderId: "499925191509"
+    apiKey: "AIzaSyBh42V4VVm_BOEhvtWfvDBNNuHdlwgAppU",
+    authDomain: "wadt-a3-izzan-raihana.firebaseapp.com",
+    databaseURL: "https://wadt-a3-izzan-raihana.firebaseio.com",
+    projectId: "wadt-a3-izzan-raihana",
+    storageBucket: "wadt-a3-izzan-raihana.appspot.com",
+    messagingSenderId: "641704138863"
   }
 };
